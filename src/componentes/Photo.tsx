ef39607ba5410e9fee89a1c202f6d38f5cb97b7f
Rodrigo <rodrigonpi@hotmail.com>
2022-07-19 22:11:38 -0300

    Fragment

---
+++ src/componentes/Photo.tsx
@@ -0,0 +1,17 @@
+import {ReactNode} from 'react';
+
+type Props = {
+    
+    legend: string;
+    children: ReactNode;
+}
+
+export const Photo = ({legend, children}:Props) => {
+    return(
+        <>
+            {children}
+            <p>{legend}</p>
+        </>
+    );
+}
+
