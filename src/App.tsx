0233f3a326846d808f5974d3ac297c5f42f3931c
Rodrigo <rodrigonpi@hotmail.com>
2022-07-19 21:30:39 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
 //criando componente
+const App = () => {
+  let n1: number = 10;
+  let n2: number = 20;
 
+  return (
+    <div>Multiplicar: {n1*n2}</div>
 
-/*function App(){
-  return <div>Ola Mundo</div>
+  );
+}
+
+/*const App = () => {
+  let name: string = "Rodrigo"
+
+  return (
+  <div>Ola {name}, tudo bem?</div>
+  );
 }*/
 
-const App = () =>{
+/*const App = () =>{
   return (
     <a href="https://b7web.com.br">Clique aqui!</a>
 
   );
-}
+}*/
 
 export default App;
