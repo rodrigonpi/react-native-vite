353419545a85497b22d4679dd1c3a94a038da14b
Rodrigo <rodrigonpi@hotmail.com>
2022-08-03 19:00:59 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 const App = () => {
+
+  const handleButtonClick = ()=>{
+    alert("Botao clicado!")
+  }
+
   return (
     <div>
+      <button onClick={handleButtonClick}>Clique aqui</button>
      
     </div>
 
